73c470fdca317338000b83059af78c1f30c9e347
codingcat2468 <lw-app-produktion@gmx.net>
2026-08-21 20:51:48 +0200

    base: Completely refactor ChangelogEditorDialog to use the new EditorSession classes

--- base/src/main/java/com/codingcat/changelogs/base/dialog/ui/editor/ChangelogEditorDialog.java
+++ base/src/main/java/com/codingcat/changelogs/base/dialog/ui/editor/ChangelogEditorDialog.java
@@ -1,7 +1,6 @@
 package com.codingcat.changelogs.base.dialog.ui.editor;
 
 import com.codingcat.changelogs.base.ServerChangelogs;
-import com.codingcat.changelogs.base.data.ChangelogEntry;
 import com.codingcat.changelogs.base.data.ChangelogStorage;
 import com.codingcat.changelogs.base.dialog.DialogPackets;
 import com.codingcat.changelogs.base.dialog.DialogSessionManager;
@@ -26,15 +25,11 @@ import com.github.retrooper.packetevents.protocol.nbt.NBTCompound;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Function;
 
 import static com.codingcat.changelogs.base.dialog.ui.ChangelogDialog.createLinesComponent;
 import static com.codingcat.changelogs.base.lang.TranslationSource.translatableManual;
@@ -42,110 +37,89 @@ import static net.kyori.adventure.text.Component.text;
 
 @RequiredArgsConstructor
 public class ChangelogEditorDialog implements IDialog {
-    private static final @NotNull String TITLE_KEY = "dialog.create.title";
+    private static final @NotNull Function<EditorSession, String> TITLE_KEY = s -> "dialog.editor." + s.getId() + ".title";
     private final @Getter String id = "changelog_editor";
     private final @NotNull ChangelogStorage storage;
     private final boolean useFallbackPermissions;
 
     @Override
     public @NotNull Dialog build(@NotNull IPlayer p, @NotNull DialogSessionManager sessionManager) {
-        sessionManager.startSessionIfNoneActive(this, p, EditorState::new);
-        EditorState state = sessionManager.getSessionData(p, EditorState.class);
-        List<Component> previewLines = state.mmLineSources().stream()
-                .map(MiniMessage.miniMessage()::deserialize)
-                .toList();
-        int newId = this.storage.nextUID() + 1;
+        sessionManager.startSessionIfNoneActive(this, p, () -> new EditorSession.Create(storage.nextUID()));
+        EditorSession session = sessionManager.getSessionData(p, EditorSession.class);
+        String sessionTranslation = "dialog.editor." + session.getId();
+        List<Component> previewLines = session.deserializeLines();
         List<DialogBody> body = List.of(
                 new ItemDialogBody(ItemStack.builder().type(ItemTypes.WRITABLE_BOOK).build(),
-                        new PlainMessage(translatableManual(p, "dialog.create.subtitle", text(newId)), 160),
+                        new PlainMessage(translatableManual(p, sessionTranslation + ".subtitle", text(session.getEntryUID() + 1)), 160),
                         false, false, 15, 15),
-                new PlainMessageDialogBody(new PlainMessage(translatableManual(p, "dialog.create.hint"), ChangelogDialog.LINE_WIDTH)),
-                new PlainMessageDialogBody(new PlainMessage(!previewLines.isEmpty() ? createLinesComponent(p, previewLines) : translatableManual(p, "dialog.create.empty_preview"), ChangelogDialog.LINE_WIDTH))
+                new PlainMessageDialogBody(new PlainMessage(translatableManual(p, "dialog.editor.hint"), ChangelogDialog.LINE_WIDTH)),
+                new PlainMessageDialogBody(new PlainMessage(!previewLines.isEmpty() ? createLinesComponent(p, previewLines) : translatableManual(p, "dialog.editor.empty_preview"), ChangelogDialog.LINE_WIDTH))
         );
         List<Input> inputs = List.of(
-                new Input("line", new TextInputControl(350, translatableManual(p, "dialog.create.input.contents"),
-                        true, state.currentLineSource().get(), 5000, null)),
-                new Input("author", new TextInputControl(200, translatableManual(p, "dialog.create.input.author"), true, state.authorSource().get(), 200, null))
+                new Input("line", new TextInputControl(350, translatableManual(p, "dialog.editor.input.contents"),
+                        true, session.getCurrentLine(), 5000, null)),
+                new Input("author", new TextInputControl(200, translatableManual(p, "dialog.editor.input.author"), true, session.getAuthor(), 200, null))
         );
         CommonDialogData common = new CommonDialogData(
-                translatableManual(p, TITLE_KEY),
+                translatableManual(p, TITLE_KEY.apply(session)),
                 null, false, false,
                 DialogAction.NONE, body, inputs
         );
-        ActionButton yesBtn = new ActionButton(new CommonButtonData(translatableManual(p, "dialog.create.button.publish"), null, 160), sessionManager.createSessionBasedAction(this, "publish", true));
-        ActionButton addLineBtn = new ActionButton(new CommonButtonData(translatableManual(p, "dialog.create.button.add_line"), null, 100), sessionManager.createSessionBasedAction(this, "add_line", true));
-        ActionButton cancelBtn = new ActionButton(new CommonButtonData(translatableManual(p, "dialog.create.button.cancel"), null, 100), sessionManager.createSessionBasedAction(this, "close", false));
+        ActionButton yesBtn = new ActionButton(new CommonButtonData(translatableManual(p, sessionTranslation + ".commit_button"), null, 160), sessionManager.createSessionBasedAction(this, "commit", true));
+        ActionButton addLineBtn = new ActionButton(new CommonButtonData(translatableManual(p, "dialog.editor.button.add_line"), null, 100), sessionManager.createSessionBasedAction(this, "add_line", true));
+        ActionButton cancelBtn = new ActionButton(new CommonButtonData(translatableManual(p, "dialog.editor.button.cancel"), null, 100), sessionManager.createSessionBasedAction(this, "close", false));
         return new MultiActionDialog(common, List.of(yesBtn, addLineBtn), cancelBtn, 3);
     }
 
     @Override
     public void onActionTriggered(@NotNull String action, @Nullable NBTCompound data, @NotNull IPlayer source, @NotNull DialogSessionManager sessionManager) {
-        if (action.equals("close")) {
-            sessionManager.endSession(source);
-            DialogPackets.clearDialog(source, DialogPackets.PacketPhase.PLAY);
-            return;
-        }
-        if (data != null) {
+        EditorSession session = sessionManager.getSessionData(source, EditorSession.class);
+        if (data != null && data.contains("line")) {
             String rawLine = data.getStringTagValueOrThrow("line");
             String rawAuthor = data.getStringTagValueOrThrow("author");
-            EditorState state = sessionManager.getSessionData(source, EditorState.class);
-            state.currentLineSource().set(rawLine);
-            state.authorSource().set(rawAuthor);
+            session.setCurrentLine(rawLine);
+            session.setAuthor(rawAuthor);
         }
-        if (action.equals("retry")) {
-            this.showTo(source, sessionManager, DialogPackets.PacketPhase.PLAY);
-            return;
-        }
-        if (action.equals("add_line")) {
-            if (data == null) return;
-            EditorState state = sessionManager.getSessionData(source, EditorState.class);
-            if (state.currentLineSource().get().isBlank()) {
-                this.showRetry(source, "add_empty_line", sessionManager);
-                return;
+        switch (action) {
+            case "close" -> {
+                sessionManager.endSession(source);
+                DialogPackets.clearDialog(source, DialogPackets.PacketPhase.PLAY);
             }
-            state.mmLineSources().add(state.currentLineSource().get());
-            state.currentLineSource().set("");
-            this.showTo(source, sessionManager, DialogPackets.PacketPhase.PLAY);
-            return;
+            case "retry" -> this.showTo(source, sessionManager, DialogPackets.PacketPhase.PLAY);
+            case "add_line" -> {
+                if (data == null) return;
+                if (session.getCurrentLine().isBlank()) {
+                    this.showRetry(source, "add_empty_line", session, sessionManager);
+                    return;
+                }
+                session.getRawLines().add(session.getCurrentLine());
+                session.setCurrentLine("");
+                this.showTo(source, sessionManager, DialogPackets.PacketPhase.PLAY);
+            }
+            case "commit" -> {
+                if ((useFallbackPermissions && !source.isNativeAdmin().toBooleanOrElse(false)) || (!useFallbackPermissions && !source.hasPermission(ServerChangelogs.NAMESPACE + ".command.create"))) {
+                    sessionManager.endSession(source);
+                    DialogPackets.showSimpleNotice(source, TITLE_KEY.apply(session), "dialog.editor.error.no_permission");
+                    return;
+                }
+                if (data == null) return;
+                if (session.getRawLines().isEmpty()) {
+                    this.showRetry(source, "no_lines", session, sessionManager);
+                    return;
+                }
+                try {
+                    session.commit(this.storage);
+                } catch (EditorSession.CommitException e) {
+                    this.showRetry(source, session.getId() + "." + e.getTranslationKeyPart(), session, sessionManager);
+                    return;
+                }
+                sessionManager.endSession(source);
+                DialogPackets.showSimpleNotice(source, TITLE_KEY.apply(session), "dialog.editor." + session.getId() + ".success");
+            }
         }
-        if (!action.equals("publish")) return;
-        if ((useFallbackPermissions && !source.isNativeAdmin().toBooleanOrElse(false)) || (!useFallbackPermissions && !source.hasPermission(ServerChangelogs.NAMESPACE + ".command.create"))) {
-            sessionManager.endSession(source);
-            DialogPackets.showSimpleNotice(source, TITLE_KEY, "dialog.create.error.no_permission");
-            return;
-        }
-        if (data == null) return;
-        EditorState editorState = sessionManager.getSessionData(source, EditorState.class);
-        List<Component> lines = editorState.mmLineSources().stream()
-                .map(MiniMessage.miniMessage()::deserialize)
-                .toList();
-        if (lines.isEmpty()) {
-            this.showRetry(source, "no_lines", sessionManager);
-            return;
-        }
-        String author = editorState.authorSource().get();
-        ChangelogEntry entry = new ChangelogEntry(
-                this.storage.nextUID(),
-                lines, Instant.now(),
-                !author.isBlank() ? MiniMessage.miniMessage().deserialize(author) : null,
-                new HashSet<>()
-        );
-        this.storage.storeEntry(entry);
-        sessionManager.endSession(source);
-        DialogPackets.showSimpleNotice(source, TITLE_KEY, "dialog.create.success");
     }
 
-    private void showRetry(@NotNull IPlayer source, @NotNull String errorPart, @NotNull DialogSessionManager sessionManager) {
-        DialogPackets.showSimpleNotice(source, TITLE_KEY, "dialog.create.error." + errorPart, sessionManager.createSessionBasedAction(this, "retry", false), DialogPackets.PacketPhase.PLAY);
-    }
-
-    public record EditorState(
-            @NotNull List<String> mmLineSources,
-            @NotNull AtomicReference<String> currentLineSource,
-            @NotNull AtomicReference<String> authorSource
-    ) {
-        public EditorState() {
-            this(new ArrayList<>(), new AtomicReference<>(""), new AtomicReference<>(""));
-        }
+    private void showRetry(@NotNull IPlayer source, @NotNull String errorPart, @NotNull EditorSession session, @NotNull DialogSessionManager sessionManager) {
+        DialogPackets.showSimpleNotice(source, TITLE_KEY.apply(session), "dialog.editor.error." + errorPart, sessionManager.createSessionBasedAction(this, "retry", false), DialogPackets.PacketPhase.PLAY);
     }
 }
